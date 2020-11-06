400a44b38991e1e2d2058602a1be5e3c684a686c
Rettus code <michael.rettusii@gmail.com>
2020-11-06 09:48:08 -0800

    clearing main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-$(function() {
-    baguetteBox.run('.gallery', {
-        captions: "true", // display image captions.
-        buttons: 'auto', // arrows navigation
-        fullScreen: true,
-        noScrollbars: false,
-        bodyClass: 'baguetteBox-open',
-        preload: 2,
-        animation: 'fadeIn', // fadeIn or slideIn
-        verlayBackgroundColor: 'rgba(0,0,0,.8)'
-    });
-});
+
 const form1 = document.querySelector('.feedback')
 
 form1.addEventListener('submit', e => {
